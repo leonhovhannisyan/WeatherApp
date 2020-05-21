b03525259445e3c5bc33683b7f308de2f9be5d72
Leonard Hovhannisyan <leonard.hovhannisyan@gmail.com>
2020-05-21 20:15:53 +0200

    Added weather output, app showing weather for four next days in closest timestamp when launched

--- Applicaton.py
+++ Applicaton.py
@@ -4,10 +4,23 @@ import requests
 HEIGHT = 657
 WIDTH = 1500
 
-# api.openweathermap.org/data/2.5/forecast?id={city ID}&appid=3203c7a72e5efde44ee699c88e1cc019
 
 def test_function(entry):
      print("You typed in: " + entry)
+
+def weather_output(weather, day):
+
+    try:
+        temp = weather['list'][day]['main']['temp']
+        descr = weather['list'][day]['weather'][0]['description']
+        city = weather['city']['name']
+        actualdate = weather['list'][day]['dt_txt']
+
+        output = 'City: %s \nConditions: %s \nTemperature (Celsius): %s \nDate: %s' % (city, descr, temp, actualdate)
+    except:
+        output = "Please provide correct city name or zip code"
+
+    return output
 
 def get_weather(city):
     weather_key = '3203c7a72e5efde44ee699c88e1cc019'
@@ -15,13 +28,10 @@ def get_weather(city):
     params = {'APPID' : weather_key, 'q' : city, 'units' : 'metric'}
     response = requests.get(url, params=params)
     weather = response.json()
-
-    # print(weather['list'][0]['main']['temp'])
-    # print(weather['list'][0]['weather'][0]['description'])
-    # print(weather['city']['name'])
-
-
-
+    weatherLabel['text'] = weather_output(weather, 0)
+    weatherLabel2['text'] = weather_output(weather, 8)
+    weatherLabel3['text'] = weather_output(weather, 16)
+    weatherLabel4['text'] = weather_output(weather, 24)
 
 root = tkinter.Tk()
 root.title("Weather Application")
@@ -39,7 +49,19 @@ button = tkinter.Button(topFrame, text='Check weather!', command=lambda: get_wea
 button.place(relx=0.87, rely=0, relwidth=0.12, relheight=1)
 topFrame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.1)
 
-leftFrame= tkinter.Frame(root, bd=5, bg='white')
-leftFrame.place(relx=0.1, rely=0.3, relwidth=0.3, relheight=0.6)
+bottomframe= tkinter.Frame(root, bd=5, bg='#bee7f7')
+bottomframe.place(relx=0.1, rely=0.3, relwidth=0.8, relheight=0.6)
+
+weatherLabel=tkinter.Label(bottomframe)
+weatherLabel.place(relx=0, rely=0, relwidth = 1, relheight = 0.22)
+
+weatherLabel2=tkinter.Label(bottomframe, bg='white')
+weatherLabel2.place(relx=0, rely=0.25, relwidth = 1, relheight =0.22)
+#
+weatherLabel3=tkinter.Label(bottomframe)
+weatherLabel3.place(relx=0, rely=0.5, relwidth = 1, relheight = 0.22)
+
+weatherLabel4=tkinter.Label(bottomframe, bg='white')
+weatherLabel4.place(relx=0, rely=0.75, relwidth = 1, relheight = 0.22)
 
 root.mainloop()
